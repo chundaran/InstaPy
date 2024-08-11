356da115cca9ba426a7bcf1a0f2ac52f1be74991
chundaran <114787318+chundaran@users.noreply.github.com>
2024-08-11 14:48:42 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -10,7 +10,8 @@ from instapy import set_workspace
 set_workspace(path=None)
 
 # get an InstaPy session!
-session = InstaPy()
+session = InstaPy(username="Bujair05", 
+        password="141481 SON")
 
 with smart_run(session):
     # general settings
